e0961e77f2ed5df42082a894a7b12cd89e12c6c0
goHere <clemay@gohere.tech>
2023-02-21 01:27:07 -0600

    updooter

--- js/logo-new.js
+++ js/logo-new.js
@@ -157,7 +157,7 @@ let P = [
         rotation: new THREE.Vector3(0, 0, 0),
       },
       logo: {
-        position: new THREE.Vector3(10, 15, -10),
+        position: new THREE.Vector3(10, 20, -10),
         rotation: new THREE.Vector3(-45, 0, -90),
       },
     },
@@ -349,7 +349,6 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.outerHeight);
   camera.position.z = start.camera.position.z * screenRatio;
-  //   getPixels();
 }
 function onScroll() {
   //Get percent scrolled
