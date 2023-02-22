a693829611b148baa6da35ed88e6786f9f262d7d
goHere <clemay@gohere.tech>
2023-02-22 01:48:21 -0600

    back to 120

--- js/logo-new.js
+++ js/logo-new.js
@@ -214,7 +214,7 @@ let P = [
         rotation: new THREE.Vector3(0, 0, 0),
       },
       logo: {
-        position: new THREE.Vector3(0, 0, 160),
+        position: new THREE.Vector3(0, 0, 120),
         rotation: new THREE.Vector3(0, 0, 120),
       },
     },
